8115faf5d4f5d961b55d81b544255a0299438891
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-22 13:10:12 +0300

    add class addNewUser

---
+++ LISS_test_api_v2/sign_in.py
@@ -0,0 +1,21 @@
+import requests
+from config import Base
+
+
+class AddNewUser:
+    url = Base.BASE_URL_V2
+    email = Base.USER_EMAIL
+
+    def sign_in_new_user(self, user_name, email, password, first_name, middle_name, last_name):
+        data = {
+            "username": user_name,
+            "email": email,
+            "password": password,
+            "first_name": first_name,
+            "middle_name": middle_name,
+            "last_name": last_name
+        }
+        r = requests.post(self.url + self.email + "/sign-in", json=data)
+        status_code = r.status_code
+        body = r.json()
+        return status_code, body
