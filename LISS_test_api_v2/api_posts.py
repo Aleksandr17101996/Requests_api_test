fad43ba00ee1660c4229ee1e4fdf970dea7ca20a
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-22 13:36:18 +0300

    class Posts_v2 add def get_post_pagination

---
+++ LISS_test_api_v2/api_posts.py
@@ -0,0 +1,17 @@
+import requests
+from config import Base
+
+
+class Posts:
+    url = Base.BASE_URL_V2
+    email = Base.USER_EMAIL
+
+    def get_post_pagination(self, number_page):
+        params = {
+            "page": number_page
+        }
+        r = requests.get(self.url + self.email + "/posts", params=params)
+        status_code = r.status_code
+        body = r.json()
+        assert status_code, body
+
