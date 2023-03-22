683678bba72222beaa38ed9191877b3e7307514d
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-22 17:26:23 +0300

    add def get_comment

--- LISS_test_api_v2/api_comments.py
+++ LISS_test_api_v2/api_comments.py
@@ -6,7 +6,7 @@ class Comments:
     url = Base.BASE_URL_V2
     email = Base.USER_EMAIL
 
-    def post_comments(self, title, text, post_id, auth):
+    def post_comment(self, title, text, post_id, auth):
         data = {
             "title": title,
             "text": text,
@@ -19,6 +19,12 @@ class Comments:
 
     def get_comments(self):
         r = requests.get(self.url + self.email + "/comments")
+        status_code = r.status_code
+        body = r.json()
+        return status_code, body
+
+    def get_comment(self, id_comment):
+        r = requests.get(self.url + self.email + "/comment" + id_comment)
         status_code = r.status_code
         body = r.json()
         return status_code, body
