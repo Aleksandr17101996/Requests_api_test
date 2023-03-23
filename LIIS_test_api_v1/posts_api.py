71cf2cae37a5613b49fe1b76d620d5a52f79ce54
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-23 22:41:11 +0300

    add Description

--- LIIS_test_api_v1/posts_api.py
+++ LIIS_test_api_v1/posts_api.py
@@ -6,13 +6,23 @@ class Posts(Base):
     url = Base.BASE_URL_V1
     email = Base.USER_EMAIL
 
+    """api библиотека для форума"""
+
     def get_post(self):
+
+        """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате
+                      JSON с созданами постами, найденных по email"""
+
         r = requests.get(self.url + self.email + "/posts")
         status_code = r.status_code
         body = r.json()
         return status_code, body
 
     def post_posts(self, title, content, auth):
+
+        """Метод отправляет (постит) на сервер данные поста и возвращает статус запроса
+                    и результат в формате JSON с данными о добавленном посте"""
+
         data = {
             "title": title,
             "content": content
@@ -23,12 +33,20 @@ class Posts(Base):
         return status_code, body
 
     def get_new_post(self, post_id):
+
+        """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате
+                      JSON с созданным постом, найденных по id"""
+
         r = requests.get(self.url + self.email + "/post/" + post_id)
         status_code = r.status_code
         body = r.json()
         return status_code, body
 
     def put_post(self, post_id, title, content, auth):
+
+        """Метод отправляет запрос на сервер о обновлении данных поста найденного по id
+        и возвращает статус запроса и результат в формате JSON с данными уведомлениями о успешном обновлении"""
+
         data = {
             "title": title,
             "content": content
@@ -39,6 +57,10 @@ class Posts(Base):
         return status_code, body
 
     def delete_post(self, post_id, auth):
+
+        """Метод отправляет на сервер запрос на удаление поста по указанному ID и возвращает
+                статус запроса и результат в формате JSON с текстом уведомления о успешном удалении"""
+
         r = requests.delete(self.url + self.email + '/post/' + post_id, auth=auth)
         status_code = r.status_code
         return status_code
