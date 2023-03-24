b792542ee6fa4ede01492f69ad79160aa3b38694
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-24 18:33:55 +0300

    update

--- LIIS_test_api_v1/comments_api.py
+++ LIIS_test_api_v1/comments_api.py
@@ -10,7 +10,7 @@ class Comments(Base):
 
     def get_comments(self):
         """Метод делает запрос на сервера и возвращает статус запроса и результат в формате
-                JSON с добавленными коментариями пользователя, найденных по email"""
+           JSON с добавленными коментариями пользователя, найденных по email"""
 
         r = requests.get(self.url + self.email + "/comments")
         status_code = r.status_code
@@ -19,7 +19,7 @@ class Comments(Base):
 
     def posts_comment(self, title, content, post_id, auth):
         """Метод отправляет (постит) на сервер коменнтарий к посту найденного по id и возвращает статус запроса
-             и результат в формате JSON с данными о добавленном комментарии"""
+           и результат в формате JSON с данными о добавленном комментарии"""
 
         data = {
             "title": title,
@@ -33,7 +33,7 @@ class Comments(Base):
 
     def get_comment(self, id_comment):
         """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате
-                              JSON с добавленным комментарием, найденного по id"""
+           JSON с добавленным комментарием, найденного по id"""
 
         r = requests.get(self.url + self.email + "/comment/" + id_comment)
         status_code = r.status_code
@@ -42,7 +42,7 @@ class Comments(Base):
 
     def put_comment(self, id_comment, title, content, auth):
         """Метод отправляет запрос на сервер о обновлении данных комментария к посту найденного по id
-         и возвращает статус запроса и результат в формате JSON с данными уведомлениями о успешном обновлении"""
+           и возвращает статус запроса и результат в формате JSON с данными уведомлениями о успешном обновлении"""
 
         data = {
             "title": title,
@@ -55,7 +55,7 @@ class Comments(Base):
 
     def delete_comment(self, id_comment, auth):
         """Метод отправляет на сервер запрос на удаление комментария к посту по указанному ID и возвращает
-                        статус запроса и результат в формате JSON с текстом уведомления о успешном удалении"""
+           статус запроса и результат в формате JSON с текстом уведомления о успешном удалении"""
 
         r = requests.delete(self.url + self.email + "/comment/" + id_comment, auth=auth)
         status_code = r.status_code
