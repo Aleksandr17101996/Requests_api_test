a923e1d16e2ff5245cd2b6c3bcc46d7ab24cfaa5
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-24 15:14:18 +0300

    add test_add_existin_email_user

--- LIIS_test_api_v1/tests/sign_in_test.py
+++ LIIS_test_api_v1/tests/sign_in_test.py
@@ -7,11 +7,34 @@ class TestNewUser(NewUser):
         """Метод содержит позитивный тестовый сценарий выполняя запрос на регистрацию пользователя
                        с валидными данными"""
 
-        user_name = "bobo0"
-        user_email = "bobo0@mail.com"
+        user_name = "Alexandr"
+        user_email = "Alexandr@mail.com"
         user_password = "123"
         status_code, body = self.sign_in_new_user(user_name, user_email, user_password)
         assert status_code == 201, "Статус кода не соответсвует ожидаемому"
         assert body["username"] == user_name, "Имя пользователя в ответе неверно"
         assert body["email"] == user_email, "Email пользователя в ответе не верен"
         assert int(body["id"]) > 0, "id не является числом"
+
+    def test_add_existin_name_user(self):
+        """Метод содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
+                               с именем уже зарегестрированного пользователя"""
+
+        user_name = "Alexandr"  # Имя Alexandr уже есть в системе
+        user_email = "Alexandr@mail.com"  # Данный емайл рандомный
+        user_password = "123"
+        status_code, body = self.sign_in_new_user(user_name, user_email, user_password)
+        assert status_code == 409, "Статус кода не соответсвует ожидаемому"
+        assert body["message"] == "User with this username or email already exists"
+
+    def test_add_existin_email_user(self):
+        """Метод содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
+                               с почтой уже зарегестрированного пользователя"""
+
+        user_name = "Alexandr"  # Имя рандомное
+        user_email = "Alexandr@mail.com"  # Данный емайл уже зарегестрированн в системе
+        user_password = "123"
+        status_code, body = self.sign_in_new_user(user_name, user_email, user_password)
+        assert status_code == 409, "Статус кода не соответсвует ожидаемому"
+        assert body["message"] == "User with this username or email already exists"
+
