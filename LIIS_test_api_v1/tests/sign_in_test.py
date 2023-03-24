870d2dd04c215fc2e6789126430d60d336f538a5
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-24 16:17:59 +0300

    add test_not_valid_password

--- LIIS_test_api_v1/tests/sign_in_test.py
+++ LIIS_test_api_v1/tests/sign_in_test.py
@@ -1,40 +1,56 @@
 from LIIS_test_api_v1.sign_in import NewUser
+from generator.generator import generated_person
 
 
 class TestNewUser(NewUser):
 
     def test_add_new_user(self):
-        """Метод содержит позитивный тестовый сценарий выполняя запрос на регистрацию пользователя
-                       с валидными данными"""
+        """Тест содержит позитивный тестовый сценарий выполняя запрос на регистрацию пользователя
+                       с валидными данными, которые сгенерированы автоматически"""
 
-        user_name = "Alexandr"
-        user_email = "Alexandr@mail.com"
-        user_password = "123"
-        status_code, body = self.sign_in_new_user(user_name, user_email, user_password)
+        person_info = next(generated_person())
+        email = person_info.email
+        password = person_info.password
+        name = person_info.user_name
+        status_code, body = self.sign_in_new_user(name, email, str(password))
         assert status_code == 201, "Статус кода не соответсвует ожидаемому"
-        assert body["username"] == user_name, "Имя пользователя в ответе неверно"
-        assert body["email"] == user_email, "Email пользователя в ответе не верен"
+        assert body["username"] == name, "Имя пользователя в ответе неверно"
+        assert body["email"] == email, "Email пользователя в ответе не верен"
         assert int(body["id"]) > 0, "id не является числом"
 
     def test_add_existin_name_user(self):
-        """Метод содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
-                               с именем уже зарегестрированного пользователя"""
+        """ Тест содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
+            с именем уже зарегестрированного пользователя, почта и пароль генерируются автоматически"""
 
-        user_name = "Alexandr"  # Имя Alexandr уже есть в системе
-        user_email = "Alexandr@mail.com"  # Данный емайл рандомный
-        user_password = "123"
-        status_code, body = self.sign_in_new_user(user_name, user_email, user_password)
+        person_info = next(generated_person())
+        email = person_info.email
+        password = person_info.password
+        user_name = "Alexandr"  # Имя Alexandr уже зарегестрированно в системе
+        status_code, body = self.sign_in_new_user(user_name, email, str(password))
         assert status_code == 409, "Статус кода не соответсвует ожидаемому"
         assert body["message"] == "User with this username or email already exists"
 
     def test_add_existin_email_user(self):
-        """Метод содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
+        """Тест содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
                                с почтой уже зарегестрированного пользователя"""
 
-        user_name = "Alexandr"  # Имя рандомное
-        user_email = "Alexandr@mail.com"  # Данный емайл уже зарегестрированн в системе
-        user_password = "123"
-        status_code, body = self.sign_in_new_user(user_name, user_email, user_password)
+        person_info = next(generated_person())
+        name = person_info.user_name
+        password = person_info.password
+        user_email = "Alexandr@mail.com"  # Данная почта уже зарегестрированна в системе.
+        status_code, body = self.sign_in_new_user(name, user_email, str(password))
         assert status_code == 409, "Статус кода не соответсвует ожидаемому"
         assert body["message"] == "User with this username or email already exists"
+
+    def test_not_valid_password(self):
+        """Тест содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
+                с паролем в формате int, и данными которые сгенерированы автоматически"""
+
+        person_info = next(generated_person())
+        email = person_info.email
+        password = person_info.password
+        name = person_info.user_name
+        status_code, body = self.sign_in_new_user(name, email, password)
+        assert status_code == 422, "Статус кода не соответсвует ожидаемому"
+        assert body["password"][-1] == "Not a valid string.", "Ошибка не обработана"
 
--- generator/generator.py
+++ generator/generator.py
@@ -3,13 +3,13 @@ import random
 from data.data import Person
 from faker import Faker
 
-faker_en = Faker('en_EN')
+faker_en = Faker('en_US')
 Faker.seed()
 
 
 def generated_person():
     yield Person(
         user_name=faker_en.last_name_female(),
-        password=random.randint(10, 80),
+        password=random.randint(100, 9999),
         email=faker_en.email(),
     )
