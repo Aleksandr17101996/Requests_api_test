4ed49094082fef964f26eeb4b283d3c1a1d5489b
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-24 14:34:03 +0300

    add test_add_new_user

--- LIIS_test_api_v1/tests/sign_in_test.py
+++ LIIS_test_api_v1/tests/sign_in_test.py
@@ -1,5 +1,17 @@
 from LIIS_test_api_v1.sign_in import NewUser
 
-class TestNewUser:
+
+class TestNewUser(NewUser):
 
     def test_add_new_user(self):
+        """Метод содержит позитивный тестовый сценарий выполняя запрос на регистрацию пользователя
+                       с валидными данными"""
+
+        user_name = "bobo0"
+        user_email = "bobo0@mail.com"
+        user_password = "123"
+        status_code, body = self.sign_in_new_user(user_name, user_email, user_password)
+        assert status_code == 201, "Статус кода не соответсвует ожидаемому"
+        assert body["username"] == user_name, "Имя пользователя в ответе неверно"
+        assert body["email"] == user_email, "Email пользователя в ответе не верен"
+        assert int(body["id"]) > 0, "id не является числом"
