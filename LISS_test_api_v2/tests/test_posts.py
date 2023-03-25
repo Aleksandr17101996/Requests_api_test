dc73f66607bb5ec1b9a8cdbf66a130b5ed298f71
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-25 15:09:29 +0300

    add def test_delete_non_existent_post

--- LISS_test_api_v2/tests/test_posts.py
+++ LISS_test_api_v2/tests/test_posts.py
@@ -1,7 +1,7 @@
 from LISS_test_api_v2.api_posts import Posts
 from requests.auth import HTTPBasicAuth
 from config import Base, GlobalErrorMessages
-from generator.generator import generate_random_string
+from generator.generator import generate_random_string, generate_random_id
 
 
 class TestPosts(Posts):
@@ -92,4 +92,51 @@ class TestPosts(Posts):
         post_id = self.test_get_posts()
         status_code, headers = self.delete_post(post_id, self.auth_admin)
         assert status_code == 204
+        assert headers["Content-Type"] == "application/json"
+
+    def test_post_posts_invalid_user(self):
+        """ Проверяем что  при отправке запроса на сервер с авторизацией польователя с неверным паролем
+            о добавлении поста возвращается статус кода 401, возвразщается обработанная ошибка с описанием """
+
+        title = generate_random_string(4)
+        content = generate_random_string(10)
+        status_code, body = self.post_post(title, content, self.auth_user_incorrect_pass)
+        assert status_code == 401
+        assert body["message"] == "Could not verify your login!"
+
+    def test_put_post_invalid_user(self):
+        """ Проверяем что запроса на сервер с авторизацией польователя с неверным паролем
+            о обновлении поста возвращается статус кода 401, возвразщается обработанная ошибка с описанием """
+
+        update_title = generate_random_string(4)
+        update_content = generate_random_string(10)
+        post_id = self.test_get_posts()
+        status_code, body = self.put_post(post_id, update_title, update_content, self.auth_user_incorrect_pass)
+        assert status_code == 401
+        assert body["message"] == "Could not verify your login!"
+
+    def test_delete_post_invalid_user(self):
+        """ Проверяем что при отправке запроса на удаление поста найденного по id,
+            пользователем с непраильныим паролем, возвращается статус кода 401"""
+        post_id = self.test_get_posts()
+        status_code, headers = self.delete_post(post_id, self.auth_user_incorrect_pass)
+        assert status_code == 401
+        assert headers["Content-Type"] == "application/json"
+
+    def test_put_non_existent_post(self):
+        """ Проверяем что  на запрос о обнавление данных в несуществующем посте c авторизацией пользователя,
+            возвращается статус кода 404 и в теле ответа содержатся данные о ошибке"""
+
+        update_title = generate_random_string(4)
+        update_content = generate_random_string(10)
+        status_code, body = self.put_post(generate_random_id(), update_title, update_content, self.auth_user)
+        assert status_code == 404, GlobalErrorMessages.WRONG_STATUS_CODE.value
+        assert body["message"] == "Post not found", GlobalErrorMessages.WRONG_VALIDATION.value
+
+    def test_delete_non_existent_post(self):
+        """ Проверяем что  на запрос о удалении несуществующего поста с авторизацией модератора
+            возвращается статус кода 404"""
+
+        status_code, headers = self.delete_post(generate_random_id(), self.auth_admin)
+        assert status_code == 404, GlobalErrorMessages.WRONG_STATUS_CODE.value
         assert headers["Content-Type"] == "application/json"
