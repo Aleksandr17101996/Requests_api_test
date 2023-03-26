a54020c9775bc23a2b243d886a564b1e742e6117
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-26 22:34:07 +0300

    revork ErrorMessages

--- LIIS_test_api_v1/tests/sign_in_test.py
+++ LIIS_test_api_v1/tests/sign_in_test.py
@@ -1,6 +1,6 @@
 from LIIS_test_api_v1.sign_in import NewUser
 from data.generator import generated_person
-from config import GlobalErrorMessages
+from config import ErrorMessages
 from jsonschema import validate
 from data.schemas.schemas import NEW_USER_SCHEMA
 
@@ -17,10 +17,10 @@ class TestNewUser(NewUser):
         name = person_info.user_name
         status_code, body = self.sign_in_new_user(name, email, str(password))
         validate(body, NEW_USER_SCHEMA)
-        assert status_code == 201, GlobalErrorMessages.WRONG_STATUS_CODE.value
-        assert body["username"] == name, GlobalErrorMessages.WRONG_BODY.value
-        assert body["email"] == email, GlobalErrorMessages.WRONG_BODY.value
-        assert int(body["id"]) > 0, GlobalErrorMessages.WRONG_BODY.value
+        assert status_code == 201, ErrorMessages.WRONG_STATUS_CODE.value
+        assert body["username"] == name, ErrorMessages.WRONG_BODY.value
+        assert body["email"] == email, ErrorMessages.WRONG_BODY.value
+        assert int(body["id"]) > 0, ErrorMessages.WRONG_BODY.value
 
     def test_add_existin_name_user(self):
         """ Тест содержит негативный тестовый сценарий выполняя запрос на регистрацию пользователя
@@ -31,7 +31,7 @@ class TestNewUser(NewUser):
         password = person_info.password
         user_name = "Alexandr"  # Имя Alexandr уже зарегестрированно в системе
         status_code, body = self.sign_in_new_user(user_name, email, str(password))
-        assert status_code == 409, GlobalErrorMessages.WRONG_STATUS_CODE.value
+        assert status_code == 409, ErrorMessages.WRONG_STATUS_CODE.value
         assert body["message"] == "User with this username or email already exists"
 
     def test_add_existin_email_user(self):
@@ -43,7 +43,7 @@ class TestNewUser(NewUser):
         password = person_info.password
         user_email = "Alexandr@mail.com"  # Данная почта уже зарегестрированна в системе.
         status_code, body = self.sign_in_new_user(name, user_email, str(password))
-        assert status_code == 409, GlobalErrorMessages.WRONG_STATUS_CODE.value
+        assert status_code == 409, ErrorMessages.WRONG_STATUS_CODE.value
         assert body["message"] == "User with this username or email already exists"
 
     def test_not_valid_password(self):
@@ -55,5 +55,5 @@ class TestNewUser(NewUser):
         password = person_info.password
         name = person_info.user_name
         status_code, body = self.sign_in_new_user(name, email, password)
-        assert status_code == 422, GlobalErrorMessages.WRONG_STATUS_CODE.value
-        assert body["password"][-1] == "Not a valid string.", GlobalErrorMessages.WRONG_VALIDATION.value
+        assert status_code == 422, ErrorMessages.WRONG_STATUS_CODE.value
+        assert body["password"][-1] == "Not a valid string.", ErrorMessages.WRONG_VALIDATION.value
