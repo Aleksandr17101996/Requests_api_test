c0c0788ddc5fa1afcb9fa5ded78dd97c75df6e07
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-03-26 14:55:40 +0300

    add test_grant_admin_role

---
+++ LISS_test_api_v2/tests/test_grant_admin_role.py
@@ -0,0 +1,18 @@
+from LISS_test_api_v2.grant_user_admin_role import GrantUserAdminRole
+from LISS_test_api_v2.tests.sign_in_test import TestNewUser
+from requests.auth import HTTPBasicAuth
+from config import Base, GlobalErrorMessages
+
+tu = TestNewUser()
+
+
+class TestGrantUserAdminRole(GrantUserAdminRole):
+    user_id = tu.test_add_new_user()
+    auth_admin = HTTPBasicAuth(Base.USER_NAME_ADMIN, Base.USER_PASSWORD_ADMIN)
+
+    def test_grant_admin_role(self):
+        """Тест проверяет возможность предоставить пользователю права администратора"""
+
+        status_code, body = self.put_admin_rights_user(self.user_id, self.auth_admin)
+        assert status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
+        assert body["messange"] == "update", GlobalErrorMessages.WRONG_BODY.value
